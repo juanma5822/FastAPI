939b67144781042309e479efb9b169da59c0eb0b
Juan Romero <juan.romero5822@gmail.com>
2022-07-18 16:35:55 -0500

    request_response_body

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
-from fastapi import FastAPI
+#Python
+from msilib.schema import Class
+from typing import Optional
 
+#Pydantic
+from pydantic import BaseModel
+
+#FastAPI
+from fastapi import FastAPI
+from fastapi import Body
 
 app = FastAPI()
+
+#Models
+
+class Person(BaseModel):
+    first_name: str
+    last_name: str
+    age: int
+    hair_color: Optional[str] = None
+    is_married: Optional[bool] = None
+
+
 
 @app.get("/") # path operation decorator
 def home():
     return {"Hello": "World"}
 
+# Request and Response Body
 
+@app.post("/person/new")
+def create_person(person: Person = Body(...)):
+    return person 
