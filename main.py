615ac7678735f21f2f54d5544a75702aeff2b0cd
Juan Romero <juan.romero5822@gmail.com>
2022-07-19 12:07:57 -0500

    Automatic_body

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ from typing import Optional
 from enum import Enum
 
 #Pydantic
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, PaymentCardNumber
+from pydantic.types import PaymentCardBrand
 from pydantic import Field
 
 #FastAPI
-from fastapi import FastAPI, Query
-from fastapi import Body,Path
+from fastapi import FastAPI
+from fastapi import Body,Query,Path
 
 app = FastAPI()
 
@@ -23,28 +24,75 @@ class Hair_color(Enum):
     red = "red"
 
 class Location(BaseModel):
-    city: str
-    state: str
-    country: str
+    city: str = Field(
+        ...,
+        min_length=1,
+        max_length=30,
+        example= "Bogota"
+    )
+    state: str = Field(
+        ...,
+        min_length=1,
+        max_length=30,
+        example= "Cundinamarca"
+    )
+    country: str = Field(
+        ...,
+        min_length=1,
+        max_length=30,
+        example= "Colombia"
+    )
 
 class Person(BaseModel):
     first_name: str = Field(
         ...,
         min_length=1,
-        max_length=25
+        max_length=25,
+        example="Juan"
         )
     last_name: str = Field(
         ...,
         min_length=1,
-        max_length=25
+        max_length=25,
+        example="Romero"
         )
     age: int = Field(
         ...,
         gt=17,
-        le=70
+        le=70,
+        example= 28
         )
+    email: EmailStr = Field(
+        ...,
+        title="Email",
+        description="here put your email",
+        example= "juan@gmail.com"
+    )    
     hair_color: Optional[Hair_color] = Field(default=None)
     is_married: Optional[bool] = Field(default=None)
+    
+
+class Payment(BaseModel):
+    card_number: PaymentCardNumber = Field(
+        ...,
+        title="Number Card",
+        description="number card credit",
+        example= '4915110267441320'
+    )
+
+    expiration_mont: int = Field(
+        ...,
+        gt=0,
+        le=12,
+        example=8
+    )
+
+    expiration_year: int = Field(...,example= 28)
+
+    @property
+    def brand_card(self) -> PaymentCardBrand:
+        return self.card_number.brand
+
 
 
 @app.get("/") # path operation decorator
@@ -54,8 +102,21 @@ def home():
 # Request and Response Body
 
 @app.post("/person/new")
-def create_person(person: Person = Body(...)):
-    return person
+def create_person(
+    person: Person = Body(...),
+    location: Location = Body(...),
+    payment: Payment = Body(...)
+):
+    return {
+        "person" : person,
+        "location" : location,
+        "payment" : {
+            "brand": payment.brand_card,
+            "last4": payment.card_number.last4,
+            "maskk": payment.card_number.masked,
+        }
+    }
+        
 
 
 # Validations: Query Parameters
@@ -99,13 +160,20 @@ def update_person(
         ...,
         title="Person ID",
         description="This is Person ID",
-        gt=0
+        gt=0,
+        example=45485
     ),
     person: Person = Body(...),
     Location: Location = Body(...)
 ):
-
+    
     result = person.dict()
     result.update(Location.dict())
-    return result
+    
+    return {
+        "person_id" : person_id,
+        "result" : result
+    }
+        
+    
 
